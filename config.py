864fdb830cafb84ebd72a66d263d38f1d90ef3c6
sakshampandey-mern <saksham.reactjs@gmail.com>
2023-04-26 14:51:58 +0530

    saved code

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employeedb.cmjfs9whhlss.us-east-1.rds.amazonaws.com"
+customuser = "pandey"
+custompass = "tereliye"
+customdb = "employee"
+custombucket = "addemployees-saksham"
+customregion = "us-east-1"
